cf50830f44f04b6b37ab668590bc48ff707ff53c
Miguel Martinez <miguelitalia@hotmail.com>
2018-04-27 13:45:42 +0000

    have amended bulb excersise with jquery

---
+++ bulb.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    $(".lightOn").click(function() {
+         $("#bulbImage").attr("src", "img/bulbon.gif");
+         $("h2").text("Switch the light OFF please");
+    });
+    
+    $(".lightOff").click(function() {
+        $("#bulbImage").attr("src", "img/bulboff.gif");
+        $("h2").text("Switch the light ON please");
+    })
+    
+})

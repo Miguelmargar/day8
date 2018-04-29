0e623ccf3aaf23a60650cb5c89e03e38249dd421
Miguel Martinez <miguelitalia@hotmail.com>
2018-04-29 18:26:24 +0000

    separated add file into two, one only js and other with js and jq together. Need to fix both.

---
+++ addonlygamejs/addjs.js
@@ -0,0 +1,24 @@
+let
+ansForm = document.getElementById("answerForm"),
+challenge = document.getElementById("question");
+    
+function addQuiz() {
+    let num1 = Math.floor(Math.random() * 11);
+    let num2 = Math.floor(Math.random() * 51);
+    
+    challenge.textContent = "How much is: " + num1 + " + " + num2 + "?";
+    
+    //ansForm["rightAnswer"].value = (num1 + num2);
+    }
+addQuiz();
+   /*
+function checkAnswer() {
+    if (ansForm["answer"].value == ansForm["rightAnswer"].value) {
+        alert("Correct Answer!");
+    } else {
+        alert("incorrect Answer! Better luck next time!");
+    }
+    ansForm["answer"].value = "";
+}
+   
+   */ 
